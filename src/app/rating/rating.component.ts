915dcbacabcb242037db4b3113b99f7b06f95ef3
Biemansbart <biemansbart@gmail.com>
2019-04-24 23:29:08 +0300

    added delay of 30 minutes to voting

--- src/app/rating/rating.component.ts
+++ src/app/rating/rating.component.ts
@@ -14,7 +14,10 @@ export class RatingComponent implements OnInit {
   @Output()
   ratingEvent = new EventEmitter;
   gebruikersRating: number;
-  constructor(private sanitazer: DomSanitizer, @Inject(LOCAL_STORAGE) private localStorage: StorageService) { }
+
+  constructor(private sanitazer: DomSanitizer, @Inject(LOCAL_STORAGE) private localStorage: StorageService) {
+  }
+
 
   ngOnInit() {
     console.log('rating has been made');
@@ -29,18 +32,36 @@ export class RatingComponent implements OnInit {
     }
     this.veranderRatingImg();
   }
+
   veranderRatingImg() {
+
     for (let j = 0; j < this.src.length; j++) {
       this.src[j] = this.sanitazer.bypassSecurityTrustUrl('https://img.icons8.com/material-outlined/24/000000/christmas-star.png');
     }
     for (let j = 0; j < this.gebruikersRating; j++) {
       this.src[j] = this.sanitazer.bypassSecurityTrustUrl('https://img.icons8.com/color/24/000000/christmas-star.png');
+
     }
   }
+
   changeRating(i: number) {
-    this.gebruikersRating = i + 1;
-    console.log('uw rating bedraagt : ' + this.gebruikersRating);
-    this.ratingEvent.emit(this.gebruikersRating);
+    if (this.localStorage.get(this.jeneverNaam + '-timestamp') === null) {
+      this.gebruikersRating = i + 1;
+      console.log('uw rating bedraagt : ' + this.gebruikersRating);
+      this.localStorage.set(this.jeneverNaam + '-timestamp', new Date().getTime());
+      this.ratingEvent.emit(this.gebruikersRating);
+    } else {
+      if (new Date(this.localStorage.get(this.jeneverNaam + '-timestamp')).getTime() - new Date().getTime() >= 300000) {
+        this.gebruikersRating = i + 1;
+        console.log('uw rating bedraagt : ' + this.gebruikersRating);
+        this.localStorage.set(this.jeneverNaam + '-timestamp', new Date().getTime());
+        this.ratingEvent.emit(this.gebruikersRating);
+      }
+    }
+    {
+
+
+    }
   }
 
 }
